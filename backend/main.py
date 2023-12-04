6a3fd32466e1f954519760f1892425b4cb7b3f55
BenJenkyn <jenkynb@gmail.com>
2023-12-04 16:53:12 -0500

    removes cors

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # new import
 from finance import get_stock_history
+from lib.helperFunctions import convert_keys_to_camel_case
 
 app = Flask(__name__)
+CORS(app)  # new line
 
-
-@app.route('/stock/<ticker>', methods=['GET'])
+@app.route("/stock/<ticker>", methods=["GET"])
 def get_stock(ticker):
     print("ticker", ticker)
     data = get_stock_history(ticker)
+    data = [convert_keys_to_camel_case(item) for item in data]
     return jsonify(data)
-
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
